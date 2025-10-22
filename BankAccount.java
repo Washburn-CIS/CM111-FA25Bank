18a9aaf62603dc5801a1c0a85874098ad93e1acf
jbkmwashburn <jbkm.washburn@gmail.com>
2025-10-22 17:52:24 +0000

    created bank account class

---
+++ BankAccount.java
@@ -0,0 +1,40 @@
+public class BankAccount {
+    private int balance;        // pennies
+    private String holderName; 
+    private int number;
+
+    public String getHolderName() {
+        return holderName;
+    }
+
+    public void setHolderName(String newHolderName) {
+        holderName = newHolderName;
+    }
+
+    public void setNumber(int newNumber) {
+        number = newNumber;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    /** Returns the balance of the account in dollars
+     * 
+     * @return
+     */
+    public double getBalance() {
+        return (double)balance/100.0;
+    }
+
+    /** make withdraw in dollars */
+    public void withdraw(double amount) {
+        balance = balance - (int)(100*amount);
+    }
+
+    /** make deposit in dollars */
+    public void deposit(double amount) {
+        balance = balance + (int)(100*amount);
+    }
+
+}
