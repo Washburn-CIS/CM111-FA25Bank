a733ebb5d3cb295e05a16e15ce043923b232dda9
jbkmwashburn <jbkm.washburn@gmail.com>
2025-10-24 17:33:14 +0000

    added a second account to the demo

--- BankApplication.java
+++ BankApplication.java
@@ -1,8 +1,12 @@
 public class BankApplication {
     public static void main(String[] args) {
         BankAccount b1 = new BankAccount();
+        BankAccount b2 = new BankAccount();
         b1.setHolderName("bill");
         b1.deposit(4.40);
         System.out.println(b1.getBalance());
+        b2.setHolderName("jill");
+        b2.deposit(30);
+        System.out.println(b2.getBalance());
     }
 }
