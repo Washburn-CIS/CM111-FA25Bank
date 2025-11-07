5e7cdf3b59346b6e9ce4a7b659b593216b147bc0
jbkmwashburn <jbkm.washburn@gmail.com>
2025-11-07 18:21:51 +0000

    introduced arrays of accounts

--- BankApplication.java
+++ BankApplication.java
@@ -2,14 +2,22 @@ import java.util.Scanner;
 
 public class BankApplication {
     public static void main(String[] args) {
-        BankAccount b1 = new BankAccount();
-        BankAccount b2 = new BankAccount();
-        b1.setHolderName("bill");
-        b1.setNumber(4236);
-        b1.deposit(4.40);
-        b2.setHolderName("jill");
-        b2.setNumber(5642);
-        b2.deposit(30);
+        BankAccount[] accounts = new BankAccount[3];
+
+        accounts[0] = new BankAccount();
+        accounts[0].setHolderName("bill");
+        accounts[0].setNumber(4236);
+        accounts[0].deposit(4.40);
+        
+        accounts[1] = new BankAccount();
+        accounts[1].setHolderName("jill");
+        accounts[1].setNumber(5642);
+        accounts[1].deposit(30);
+        
+        accounts[2] = new BankAccount();
+        accounts[2].setHolderName("phil");
+        accounts[2].setNumber(1111);
+        accounts[2].deposit(5000000);
 
         Scanner input = new Scanner(System.in);
         System.out.println("Welcome to the bank app!");
@@ -25,30 +33,41 @@ public class BankApplication {
                 case 1:{
                     System.out.print("Enter Account Number: ");
                     int acct = Integer.parseInt(input.nextLine());
-                    if(acct == b1.getNumber()) {
-                        System.out.println("Balance: $" + b1.getBalance());
-                    } else {
-                        System.out.println("Balance: $" + b2.getBalance());
+                    for(int i=0; i<accounts.length; i++) {
+                        if(acct == accounts[i].getNumber()) {
+                            System.out.println("Balance: $" + accounts[i].getBalance());
+                        } 
                     }
                     break;
                 } case 2:{
                     System.out.print("Enter Account Number: ");
                     int acct = Integer.parseInt(input.nextLine());
-                    BankAccount b;
-                    if(acct == b1.getNumber()) {
-                        b = b1;
-                    } else {
-                        b = b2;
+                    BankAccount b = null;
+                    for(int i=0; i<accounts.length; i++) {
+                        if(acct == accounts[i].getNumber()) {
+                            b = accounts[i];
+                        } 
                     }
-
                     System.out.println("Enter deposit ammount (in dollars)");
                     double ammount = Double.parseDouble(input.nextLine());
                     b.deposit(ammount);
                     System.out.println("done!");
                     break;
-                } case 3:
+                } case 3:{
+                    System.out.print("Enter Account Number: ");
+                    int acct = Integer.parseInt(input.nextLine());
+                    BankAccount b = null;
+                    for(int i=0; i<accounts.length; i++) {
+                        if(acct == accounts[i].getNumber()) {
+                            b = accounts[i];
+                        } 
+                    }
+                    System.out.println("Enter withdraw ammount (in dollars)");
+                    double ammount = Double.parseDouble(input.nextLine());
+                    b.withdraw(ammount);
+                    System.out.println("done!");
                     break;
-                case 4:
+                } case 4:
                     System.out.println("bye!");
                     System.exit(1);
             }
